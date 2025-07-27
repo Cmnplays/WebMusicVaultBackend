da88fcc8d6b01adf2de465777dd692008a8d5514
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2025-07-27 23:57:42 +0545

    adjustments for deployment

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,11 @@ import { env } from "./config/env";
 
 import cors from "cors";
 //*Normal middlewares
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
