c4d48684c5d0084aa73dab427fe99fa52db1af6d
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2025-08-09 23:37:32 +0545

    Minor adjustments

--- src/controllers/song.controller.ts
+++ src/controllers/song.controller.ts
@@ -6,6 +6,7 @@ import { HttpStatus } from "../utils/HttpStatus";
 import ApiResponse from "../utils/ApiResponse";
 import ApiError from "../utils/ApiError";
 import { UploadApiResponse } from "cloudinary";
+import type { SortOrder } from "mongoose";
 
 const uploadSongs = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
@@ -57,11 +58,17 @@ const getAllSongs = asyncHandler(
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
-    const sortBy = req.query.sortBy
-      ? req.query.sortBy === "asc"
-        ? 1
-        : -1
-      : -1;
+    const sortByValue = req.query.SortOrder as string | undefined;
+    let sortBy: SortOrder;
+    if (!sortByValue) {
+      sortBy = -1; // descending
+    } else {
+      if (sortByValue === "asc") {
+        sortBy = 1; // ascending
+      } else {
+        sortBy = -1; // descending
+      }
+    }
 
     const songs = await Song.find()
       .select("title duration fileUrl")
