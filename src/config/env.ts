f671d58d781ce60f4f55b3a4e39e17da77ad96cf
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2025-08-21 14:26:49 +0545

    Removed unnecessary thing

--- src/config/env.ts
+++ src/config/env.ts
@@ -25,7 +25,6 @@ const envSchema = z.object({
   CLOUDINARY_API_KEY: z.string().min(1, "Cloudinary API key is required"),
   CLOUDINARY_API_SECRET: z.string().min(1, "Cloudinary API secret is required"),
   FRONTEND_URL: z.string(),
-  SONG_FOLDER_PATHS: z.string(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
