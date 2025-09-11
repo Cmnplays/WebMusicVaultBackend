8d85f0f43a0ed089c5707fcf369e82a1f1997e9b
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2025-09-11 16:13:22 +0545

    Updated search song service to also accept pagination params

--- src/controllers/song.controller.ts
+++ src/controllers/song.controller.ts
@@ -121,10 +121,15 @@ const deleteSongById = asyncHandler(
 
 const searchSong = asyncHandler(
   async (req: Request, res: Response): Promise<void> => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
     const query = req.query.searchQuery as string;
     const searchedSongs = await Song.find({
       title: { $regex: query, $options: "i" },
-    }).limit(10);
+    })
+      .skip(skip)
+      .limit(limit);
     res
       .status(HttpStatus.OK)
       .json(
