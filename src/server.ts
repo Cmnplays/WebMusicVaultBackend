23c8f9b44f7b7a6c6a1f1848bb0b543dbe8c1ea4
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2025-10-02 17:03:14 +0545

    Added direct uploader feature so that one can add songs in bulk instead of 1 or 2 songs at a time.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import app from "./app";
 import connectDb from "./config/config";
 import { env } from "./config/env";
+import directUploader from "./scripts/directUploadFromSystem";
 
 const startServer = async () => {
   try {
     await connectDb();
     app.listen(env.PORT);
+    // directUploader("E:/Personal Folders/Music/musics");-->give path of folder in which your songs are present
   } catch (error) {
     if (error instanceof Error) {
       console.log("Error while starting the server: ", error.message, error);
