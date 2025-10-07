8f334636a9004c803745d2e8e549a8d419e57171
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2025-10-07 22:33:22 +0545

    Added mongodb docs testing files

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
+import { check } from "zod";
 import app from "./app";
 import connectDb from "./config/config";
 import { env } from "./config/env";
 // import directUploader from "./scripts/directUploadFromSystem";
-
+// import { checkDbData, checkDuplicateFiles } from "./scripts/mongodbfiletest";
 const startServer = async () => {
   try {
     await connectDb();
     app.listen(env.PORT);
-    // directUploader("E:/Personal Folders/Music/musics/hindi"); //-->give path of folder in which your songs are present
+    //checkDbData();
+    //checkDuplicateFiles();
+    //directUploader("E:/Personal Folders/Music/musics/hindi"); //-->give path of folder in which your songs are present
+    console.log("Script ran successfully");
   } catch (error) {
     if (error instanceof Error) {
       console.log("Error while starting the server: ", error.message, error);
