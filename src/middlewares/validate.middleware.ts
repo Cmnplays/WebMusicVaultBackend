8ca5bb312f431c22ad57e5d666f4f08e7a0288d5
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2026-01-08 17:16:50 +0545

    fixed validate middleware so that it can actually validate and return the validated values

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -5,7 +5,10 @@ import { ZodType, ZodError } from "zod";
 export const validate = (schema: ZodType<any>) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      schema.parse(req.body);
+      // schema.parse(req.body); while developing the version 2 (reviewing and updating  branch name of now (08,01,2026)) i noticed that i was just doing schema.parse i was not reassigning the parsed value to the req.body back
+
+      const parsed = schema.parse(req.body);
+      req.body = parsed;
       next();
     } catch (err) {
       if (err instanceof ZodError) {
